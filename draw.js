45990b24e01578d03aff601a03141beb98a197c7
Jon Odom <odomj5@gmail.com>
2020-03-27 12:05:04 -0700

    Added spacebar to start function

--- draw.js
+++ draw.js
@@ -3,11 +3,11 @@ const context = canvas.getContext("2d");
 const size = 20;
 const row = canvas.height / size;
 const col = canvas.width / size;
-let isPaused = false
+let isPaused = true;
 
 let snake;
 
-(function setup() {
+function setup() {
     snake = new Snake();
     food = new Food();
 
@@ -35,9 +35,69 @@ let snake;
 
     }, 100);
 
-}());
+};
+
+// (function setup() {
+//     snake = new Snake();
+//     food = new Food();
+
+//     food.randLocation();
+
+//     const play = window.setInterval(() => {
+//         snake.update();
+//         food.draw();
+//         // food.drawImage()  
+//         snake.draw();
+
+//         if (snake.eat(food)) {
+//             food.randLocation();
+//         }
+
+//         if (isPaused) {
+//             clearInterval(play)
+//         }
+
+
+//         snake.collide()
+
+//         document.querySelector(".info-box")
+//             .innerText = `Your Score: ${snake.bites}`
+
+//     }, 100);
+
+// }());
+
+
+// window.addEventListener("keydown", ((evt) => {
+//     const path = evt.key.replace("Arrow", "")
+//     snake.changePath(path)
+// }))
+
+// if(isPaused) {
+//     window.addEventListener("keydown", ((evt) => {
+//     //    console.log(evt.code)
+//        if(evt.code === "Space") {
+//            setup()
+//            isPaused = false
+//        }
+//     }))
+// } else {
+//     window.addEventListener("keydown", ((evt) => {
+//         const path = evt.key.replace("Arrow", "")
+//         snake.changePath(path)
+//     }))
+// }
+
 
 window.addEventListener("keydown", ((evt) => {
-    const path = evt.key.replace("Arrow", "")
-    snake.changePath(path)
+
+    if(evt.code === "Space" && isPaused) {
+        setup()
+        isPaused = false
+    } else {
+        const path = evt.key.replace("Arrow", "")
+        snake.changePath(path)
+    }
+
+    
 }))
