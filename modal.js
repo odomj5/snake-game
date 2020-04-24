3259ce699a774c226a409374ba3399234a41d659
Jon Odom <odomj5@gmail.com>
2020-04-24 13:17:02 -0700

    Positioned intructions over game canvas

--- modal.js
+++ modal.js
@@ -1,8 +1,8 @@
 // Get the modal
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
